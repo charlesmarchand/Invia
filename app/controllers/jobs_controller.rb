fb81fe4463fb6aaf166039dbef55d78b6fe67db4
Martin Dys <martin.dys@edhec.com>
2018-10-30 15:59:49 +0100

    pull

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,15 @@
 class JobsController < ApplicationController
   def index
-   if params[:query].present?
-      @keywords = Keyword.where("name ILIKE ?", "%#{params[:query]}%")
+
+    if (params[:interests].present?) || (params[:subjects].present?)
+      allparams = []
+      if params[:subjects].present?
+      allparams << params[:subjects]
+      end
+      if params[:interests].present?
+      allparams << params[:interests]
+      end
+      @keywords = Keyword.where("name ILIKE ?", "%#{allparams.join(',')}%")
       @jobs = Job.joins(:jobs_keywords).where('jobs_keywords.keyword_id IN (?)', @keywords.ids)
     else
       @jobs = Job.all
