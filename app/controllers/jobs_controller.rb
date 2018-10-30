5989e26e1253d03a813a6a6aadd9ac49e99b67a1
Martin Dys <martin.dys@edhec.com>
2018-10-30 11:49:54 +0100

    input search done

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,9 +2,7 @@ class JobsController < ApplicationController
   def index
    if params[:query].present?
       @keywords = Keyword.where("name ILIKE ?", "%#{params[:query]}%")
-      @keywords.each do |keyword|
-        @jobs = Job.where(keywords: keyword.id)
-      end
+      @jobs = Job.joins(:jobs_keywords).where('jobs_keywords.keyword_id IN (?)', @keywords.ids)
     else
       @jobs = Job.all
     end
