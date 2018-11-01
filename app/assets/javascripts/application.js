a84e32a47061b0d0fb06027fb2ac78d679578f12
Cecile Gressard <cecile.gressard@edhec.com>
2018-11-01 14:30:01 +0100

    1er nov 14h30

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
   slidesToScroll: 3,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 2000,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
@@ -20,14 +20,14 @@
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 1200,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 700,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
