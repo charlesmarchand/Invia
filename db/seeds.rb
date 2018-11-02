56f8aac36e4c969022370b2e9fef442622629ed2
Martin Dys <martin.dys@edhec.com>
2018-11-02 16:56:45 +0100

    seed diploma studies

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@ require 'csv'
 
 csv_options = { col_sep: ';'}
 
+puts 'Cleaning'
+
+Job.destroy_all
+Diploma.destroy_all
+Study.destroy_all
+
 CSV.foreach('seed-jobs.csv', csv_options) do |row|
   job = Job.create(name: "#{row[0]}", description: "#{row[2]}", url: "#{row[3]}")
   row[1].split(",").each do |keyword|
@@ -13,17 +19,19 @@ CSV.foreach('seed-jobs.csv', csv_options) do |row|
   end
 end
 
-CSV.foreach('seed-diplomas.csv', csv_options) do |row|
+CSV.foreach('seed-diplomasUTF8.csv', csv_options) do |row|
   diploma = Diploma.create(name: "#{row[0]}", description: "#{row[3]}", length: "#{row[1]}", cost: "#{row[2]}")
 end
 
+
 CSV.foreach('seed-studies.csv', csv_options) do |row|
-  job = Job.find_by_name("#{row[0]}")
+  job = Job.find_by_name(row[0])
   row[1].split(",").each do |study|
-    job.studies << Study.create(name: "#{study}")
-    study.split("+").each do |diploma|
-      study.diplomas << Diploma.find_by_name("#{diploma}")
-    end
+    stu = Study.create(name: "#{study}")
+    job.studies << stu
+      study.split("+").each do |diploma|
+        stu.diplomas << Diploma.find_by_name("#{diploma}")
+      end
   end
 end
 
