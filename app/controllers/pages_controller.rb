54271930ee4f15a219226017a28d4856672318ff
Cecile Gressard <cecile.gressard@edhec.com>
2018-11-05 17:28:43 +0100

    le dashboard marche enfin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,9 +8,14 @@ class PagesController < ApplicationController
 
   def dashboard
 
-
     @jobs = current_user.jobs
+
     render layout: 'application'
 
+  end
+
+  def dashboard_show
+    @study = Study.find(params[:id])
+    @schools = current_user.schools
   end
 end
