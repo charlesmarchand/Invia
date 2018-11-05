91c4b2b3807c03166c02dc4fcc1460e239bedfca
Charles Marchand <charles.marchand7@gmail.com>
2018-11-05 15:56:13 +0100

    fixed studies(length + difficulty)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_144431) do
+ActiveRecord::Schema.define(version: 2018_11_05_145440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,8 +109,6 @@ ActiveRecord::Schema.define(version: 2018_11_05_144431) do
   create_table "studies", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "length"
-    t.integer "difficulty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url"
