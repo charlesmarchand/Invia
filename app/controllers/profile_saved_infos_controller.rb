923a30610acf40e3f3ed3a3160da6bb81ce5421a
Martin Dys <martin.dys@edhec.com>
2018-11-06 09:46:47 +0100

    heart

--- app/controllers/profile_saved_infos_controller.rb
+++ app/controllers/profile_saved_infos_controller.rb
@@ -6,15 +6,17 @@ class ProfileSavedInfosController < ApplicationController
     @profile_saved_info.user = current_user
     @profile_saved_info.job = @job
     @profile_saved_info.study = @study
-    if @profile_saved_info.save && (@study.nil?)
-      respond_to do |format|
-        format.html { redirect_to  job_studies_path(@job) }
-        format.js
-      end
-    elsif @profile_saved_info.save
-      respond_to do |format|
-        format.html { redirect_to jobs_path }
-        format.js
+    if @profile_saved_info.save
+      if ! params[:study_id].nil?
+        respond_to do |format|
+          format.html { redirect_to  job_studies_path(@job) }
+          format.js
+        end
+      else
+        respond_to do |format|
+          format.html { redirect_to jobs_path }
+          format.js
+        end
       end
     else
       respond_to do |format|
