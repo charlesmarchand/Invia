2f20bff413b24ad95a762b62ecc48021022502af
Martin Dys <martin.dys@edhec.com>
2018-11-06 12:49:16 +0100

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ puts 'Cleaning'
 Job.destroy_all
 Diploma.destroy_all
 Study.destroy_all
+School.destroy_all
 
 CSV.foreach('seed-jobs.csv', csv_options) do |row|
   job = Job.create(name: "#{row[0]}", description: "#{row[2]}", url: "#{row[3]}")
@@ -39,10 +40,8 @@ CSV.foreach('seed-schools.csv', csv_options) do |row|
   school = School.create(name: "#{row[0]}", address: "#{row[1]}", cost: "#{row[2]}", url: "#{row[3]}", web_site: "#{row[4]}", difficulty: "#{row[5]}", description: "#{row[6]}")
 end
 
-CSV.foreach('seed-studies_schools.csv', csv_options) do |row|
-  p row[0]
-  diploma = Diploma.find_by_name("#{row[0]}")
-  p diploma
+CSV.foreach('seed-studies_schools.csv', { col_sep: ';', headers: true}) do |row|
+  diploma = Diploma.find_by_name(row[0])
   row[1].split(",").each do |school|
     if ! School.find_by_name("#{school}").nil?
      diploma.schools << School.find_by_name("#{school}")
