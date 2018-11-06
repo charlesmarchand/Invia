54428c8b4b27e8a4546350f185e2678e45d3bd47
Charles Marchand <charles.marchand7@gmail.com>
2018-11-06 10:12:49 +0100

    un peu de css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@
 import "bootstrap";
 import { autocomplete } from '../components/autocomplete';
 import { addSchools } from '../components/add_school';
-import { showSchools } from '../components/show_schools';
 import "bouton-home";
 
 autocomplete();
