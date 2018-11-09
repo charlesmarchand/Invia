21cab9a327446dda5918209ea8f468c8e2b52690
Charles Marchand <charles.marchand7@gmail.com>
2018-11-09 14:43:34 +0100

    invia

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -169,8 +169,15 @@ map.setStyle('map_style');
             });
           });
 
+          if (markers.length === 0) {
+            map.setZoom(2);
+          } else if (markers.length === 1) {
+            map.setCenter(markers[0].lat, markers[0].lng);
+            map.setZoom(10);
+          } else {
+            map.fitLatLngBounds(markers);
+          }
 
-          map.fitLatLngBounds(markers);
 
     });
   });
